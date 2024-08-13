112dfe80706f8c6c764785f292581068af63b3c4
aravinth <aravinthramchandran@gmail.com>
2024-08-13 23:02:13 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className=' text-6xl  text-blue-500'>
+      Next.js Application Deployment to Azure Static Web App
+    </div>
+  );
+}
